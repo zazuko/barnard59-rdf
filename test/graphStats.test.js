74d505f8a4dc63711de7d763a22ea57f38ec1d46
Cristian <cristianvasquez@gmail.com>
2022-04-25 11:32:58 +0200

    feat: named graph stats.
    https://github.com/zazuko/issues-private/issues/27

--- test/graphStats.test.js
+++ test/graphStats.test.js
@@ -70,4 +70,46 @@ describe('metadata.graphStats', () => {
     strictEqual(result[3].equals(metadata[0]), true)
     strictEqual(result[4].equals(metadata[1]), true)
   })
+
+  it('should append triple and entity counts to ns.void.Dataset, is named-graph is defined', async () => {
+    const data = [
+      rdf.quad(ex.dataset1, ns.rdf.type, ns._void.Dataset, ex.graph0),
+      rdf.quad(ex.cube1, ns.rdf.type, ns.cube.Cube, ex.graph1),
+      rdf.quad(ex.cube1, ex.prop1, ex.obj1, ex.graph2)
+    ]
+
+    const namedGraph = rdf.blankNode('http://test/0')
+    const counts = rdf.blankNode('http://test/1')
+
+    const metadata = [
+      rdf.quad(ex.dataset1, ns.sd.namedGraph, namedGraph, ex.graph0),
+      rdf.quad(namedGraph, ns.sd.name, rdf.namedNode('http://test'), ex.graph0),
+      rdf.quad(namedGraph, ns.sd.graph, counts, ex.graph0),
+      rdf.quad(counts, ns.rdf.type, ns.sd.Graph, ex.graph0),
+      rdf.quad(counts, ns._void.triples, rdf.literal(3), ex.graph0),
+      rdf.quad(counts, ns._void.entities, rdf.literal(2), ex.graph0)
+    ]
+
+    const step = await graphStats({
+      graph: rdf.namedNode('http://test')
+    })
+
+    const result = await getStream.array(Readable.from(data).pipe(step))
+
+    strictEqual(result.length, 9)
+    strictEqual(result[0].equals(data[0]), true)
+    strictEqual(result[1].equals(data[1]), true)
+    strictEqual(result[2].equals(data[2]), true)
+
+    strictEqual(result[3].equals(metadata[0]), true)
+    strictEqual(result[4].equals(metadata[1]), true)
+    strictEqual(result[5].equals(metadata[2]), true)
+    strictEqual(result[6].equals(metadata[3]), true)
+    strictEqual(result[7].equals(metadata[4]), true)
+    strictEqual(result[8].equals(metadata[5]), true)
+  })
+
+  /**
+   * https://www.w3.org/TR/void/#statistics
+   */
 })
