40560f38cf3c4f514648a1f72605958e3dd004bc
Cristian <cristianvasquez@gmail.com>
2023-03-21 18:00:25 +0100

    chore: make less environment variables mandatory
    https://github.com/zazuko/barnard59-rdf/pull/32

--- test/metadata/produceProv.test.js
+++ test/metadata/produceProv.test.js
@@ -33,18 +33,18 @@ const snapshot = `<https://example.org/user/pipeline> <http://www.w3.org/1999/02
 <https://example.org/user/pipeline> <http://schema.org/description> "Test pipeline" .
 <https://example.org/user/pipeline> <http://schema.org/name> "test-pipeline" .
 <https://example.org/user/pipeline/-/pipelines> <https://barnard-prov.described.at/contains> <https://example.org/user/pipeline/-/pipelines/36212> .
+<https://example.org/user/pipeline/-/pipelines/36212> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://barnard-prov.described.at/PipelineRun> .
+<https://example.org/user/pipeline/-/pipelines/36212> <https://barnard-prov.described.at/hasJob> <https://example.org/user/pipeline/-/jobs/48940> .
+<https://example.org/user/pipeline/-/pipelines/36212> <http://www.w3.org/ns/prov#startedAtTime> "2023-03-14T12:24:53+01:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<https://example.org/user/pipeline/-/jobs/48940> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Activity> .
+<https://example.org/user/pipeline/-/jobs/48940> <https://barnard-prov.described.at/startedAtTime> "2023-03-14T12:25:09+01:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<https://example.org/user/pipeline/-/jobs/48940> <https://barnard-prov.described.at/hasEnvironment> <https://barnard-prov.described.at/environment/develop> .
+<https://example.org/user/pipeline/-/jobs/48940> <http://www.w3.org/ns/prov#wasTriggeredBy> <https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> .
 <https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://barnard-prov.described.at/Commit> .
-<https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> <https://barnard-prov.described.at/triggered> <https://example.org/user/pipeline/-/pipelines/36212> .
 <https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> <http://schema.org/name> "I added the commit title" .
+<https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> <https://barnard-prov.described.at/triggered> <https://example.org/user/pipeline/-/pipelines/36212> .
 <https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> <https://barnard-prov.described.at/author> "User <user@example.org>" .
 <https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> <http://www.w3.org/ns/prov#atTime> "2023-03-14T12:24:50+01:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
-<https://example.org/user/pipeline/-/pipelines/36212> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://barnard-prov.described.at/PipelineRun> .
-<https://example.org/user/pipeline/-/pipelines/36212> <http://www.w3.org/ns/prov#startedAtTime> "2023-03-14T12:24:53+01:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
-<https://example.org/user/pipeline/-/pipelines/36212> <https://barnard-prov.described.at/hasJob> <https://example.org/user/pipeline/-/jobs/48940> .
-<https://example.org/user/pipeline/-/jobs/48940> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://barnard-prov.described.at/Activity> .
-<https://example.org/user/pipeline/-/jobs/48940> <http://www.w3.org/ns/prov#startedAtTime> "2023-03-14T12:25:09+01:00"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
-<https://example.org/user/pipeline/-/jobs/48940> <http://www.w3.org/ns/prov#wasTriggeredBy> <https://example.org/user/pipeline/-/commit/30dd92dc282586159c8d4401d26262351f7228e0> .
-<https://example.org/user/pipeline/-/jobs/48940> <https://barnard-prov.described.at/hasEnvironment> <https://barnard-prov.described.at/environment/develop> .
 `
 
 describe('provFromGitlab', () => {
